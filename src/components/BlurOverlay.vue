82aba77489edc8cd60247a331a30cb63952c332b
Leung Yau Ming <leungyauming2005@gmail.com>
2021-07-31 15:24:25 +0800

    create BlurOverlay component

---
+++ src/components/BlurOverlay.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="blur-overlay"></div>
+</template>
+
+<script>
+export default {
+  name: "BlurOverlay",
+};
+</script>
+
+<style lang="scss" scoped>
+.blur-overlay {
+  position: absolute;
+
+  width: 100vw;
+  height: 100vh;
+
+  backdrop-filter: blur(5px);
+}
+</style>
