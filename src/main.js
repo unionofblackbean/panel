82e75b9cad8dd959a08031f67c7d5f1add3df303
Leung Yau Ming <leungyauming2005@gmail.com>
2021-08-10 14:05:05 +0800

    setup vuex

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import App from "@/App";
 import router from "@/router";
 import i18n from "./i18n";
 import "./element";
+import store from "./vuex";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   i18n,
+  store,
   render: (h) => h(App),
 }).$mount("#app");
